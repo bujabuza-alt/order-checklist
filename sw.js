fb6ce5227fa50798c2f658b973d17c7280bee17a
bujabuza-alt <bujabuza@gmail.com>
2026-03-21 23:51:38 +0900

    Fix syntax issues in service worker code

--- sw.js
+++ sw.js
@@ -1,41 +1,41 @@
-const CACHE_VERSION = 'v1.0.1'; // 코드 수정할 때 이것만 올리면 됨
+const CACHE_VERSION = ‘v1.0.1’;
 const CACHE_NAME = `order-checklist-${CACHE_VERSION}`;
 
 const urlsToCache = [
-  './',
-  './index.html'
+‘./’,
+‘./index.html’
 ];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
-  self.skipWaiting(); // 즉시 활성화
+self.addEventListener(‘install’, event => {
+event.waitUntil(
+caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+);
+self.skipWaiting();
 });
 
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key)) // 이전 버전 캐시 삭제
-      )
-    )
-  );
-  self.clients.claim();
+self.addEventListener(‘activate’, event => {
+event.waitUntil(
+caches.keys().then(keys =>
+Promise.all(
+keys
+.filter(key => key !== CACHE_NAME)
+.map(key => caches.delete(key))
+)
+)
+);
+self.clients.claim();
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(cached => {
-      return fetch(event.request)
-        .then(response => {
-          const clone = response.clone();
-          caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
-          return response;
-        })
-        .catch(() => cached); // 오프라인이면 캐시 반환
-    })
-  );
+self.addEventListener(‘fetch’, event => {
+event.respondWith(
+caches.match(event.request).then(cached => {
+return fetch(event.request)
+.then(response => {
+const clone = response.clone();
+caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+return response;
+})
+.catch(() => cached);
+})
+);
 });
